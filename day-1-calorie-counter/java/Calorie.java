0083efb261a5252bc6e04d2fca190717cfb3dcba
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2022-12-01 03:05:09 -0800

    first commit

---
+++ day-1-calorie-counter/java/Calorie.java
@@ -0,0 +1,26 @@
+// import java.io.*;
+// import java.util.*;
+// import java.util.Scanner;
+// import java.nio.file.*;
+// import java.io.BufferedReader;
+// import java.io.FileReader;
+// import java.io.IOException;
+// import java.nio.charset.StandardCharsets;
+
+// class Calorie {
+//     public static void main(String[] args) {
+
+//         String fileName = "/Users/Michelle Wang/Desktop/Coding/Advent of Code 2022/day-1-calorie-counter/input.txt";
+//         Path path = Paths.get(fileName);
+//         byte[] bytes = Files.readAllBytes(path);
+//         List<String> allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
+
+//         // for (String line : Files.readAllLines(Paths.get("C:\\Users\\Michelle Wang\\Desktop\\Coding\\Advent of Code 2022\\day-1-calorie-counter\\input.txt"))) {
+//         //     System.out.println(line);
+//         // }   
+
+//     }
+// }
+
+   
+     
---
+++ day-1-calorie-counter/python/main.py
@@ -0,0 +1,31 @@
+f = open("input.txt", "r")
+
+maxCalorie = 0
+currCalorie = 0
+
+first = 0
+second = 0
+third = 0
+
+#first sol
+for line in f:
+    if line == '\n':
+        if currCalorie > maxCalorie:
+            maxCalorie = currCalorie
+        currCalorie = 0
+    else:
+        currCalorie += int(line)
+  
+print(maxCalorie)
+
+# second sol
+# for line in f:
+#     if (line.strip() == ""):
+#         maxCalorie = max(currCalorie, maxCalorie)
+#         currCalorie = 0
+#         print("new max is " + str(maxCalorie))
+#         continue
+#     currCalorie += int(line)
+#     print("The current calories " + str(currCalorie))
+
+# print("The max calories is " + str(maxCalorie))
