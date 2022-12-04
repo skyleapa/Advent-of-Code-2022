3650a420fbb0ad0b175250b61316a1f7cfc27db3
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2022-12-03 23:39:53 -0800

    day4part1 in progress
    Co-authored-by: Darryl Tanzil <darryltanzil@gmail.com>

---
+++ day-4-camp-cleanup/camp-cleanup-1.py
@@ -0,0 +1,28 @@
+f = open("input_test.txt", "r")
+
+# logic: for example with 2-8,3-7, evaluate as a-b,c-d
+# get the values a, b, c, d. 
+# if a == c or b == d, pairsContained++
+# if a<c, then if d<b, pairsContained++
+# if a>c, then if d>b, pairsContained++
+
+pairsContained = 0
+
+for line in f:
+    first_half = line.split(',')[0]
+    second_half = line.split(',')[1]
+    a = first_half.split()[0]
+    b = first_half.split("-")[1]
+    c = first_half.split
+    d = 
+    
+    if a == c or b == d:
+        pairsContained += 1
+    elif a < c:
+        if d < b:
+            pairsContained += 1
+    elif a > c:
+        if d > b:
+            pairsContained += 1
+
+print(pairsContained)
