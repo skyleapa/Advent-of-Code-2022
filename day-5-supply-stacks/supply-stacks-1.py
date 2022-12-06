a73ca019ae1763af49a63329c6114cf574116a4e
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2022-12-06 03:02:25 -0800

    day 5 part 1 solution cleaned up

--- day-5-supply-stacks/supply-stacks-1.py
+++ day-5-supply-stacks/supply-stacks-1.py
@@ -1,29 +1,22 @@
 import re
 
+# runtime?
+
 f = open("input_michelle.txt", "r")
 
 stackHolder = [[], [], [], [], [], [], [], [], []]
 
-stopper = 0
-
 def getElements(line):
     return [int(s) for s in re.findall(r'\b\d+\b', line)]
 
-# amount is 0 index
 def stackMover(amount, startIndex, endIndex):
     for x in range(0, amount):
-        # print(stackHolder[startIndex])
-        # print(stackHolder[endIndex])
-        # stackHolder[endIndex].insert(0, stackHolder[startIndex][0])
-        # stackHolder[startIndex].pop(0)
-        # print(stackHolder[startIndex])
-        # print(stackHolder[endIndex])
-
         stackHolder[endIndex].insert(0, stackHolder[startIndex][0])
         stackHolder[startIndex].pop(0)
 
+stopper = 0
 for line in f:
-    # building stacks)
+    # building stacks
     index = 0
     if stopper < 8:
         # by column
@@ -35,12 +28,11 @@ for line in f:
     # working with move-to-from instructions
     elif stopper == 10:
         elements = getElements(line)
-        # print(elements)
         stackMover(elements[0], elements[1] - 1, elements[2] - 1)
     # so we can access the txt file after 2 lines
     else:
         stopper += 1
 
-# print(stackHolder)
+# final solution
 for x in range(0, 9):
     print(stackHolder[x][0])
