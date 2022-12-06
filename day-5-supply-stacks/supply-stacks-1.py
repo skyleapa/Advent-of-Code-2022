9e36e6ee2a4969ab376cec8100e35dd45e0739ab
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2022-12-06 02:43:48 -0800

    implementing stackMover

--- day-5-supply-stacks/supply-stacks-1.py
+++ day-5-supply-stacks/supply-stacks-1.py
@@ -9,9 +9,12 @@ stopper = 0
 def getElements(line):
     return [int(s) for s in re.findall(r'\b\d+\b', line)]
 
+# amount is 0 index
 def stackMover(amount, startIndex, endIndex):
+    # print(stackHolder[endIndex][0])
     for x in range(0, amount):
-        
+        stackHolder[endIndex].insert(0, stackHolder[startIndex][0])
+        stackHolder[startIndex].pop(0)
 
 for line in f:
     # building stacks
@@ -20,14 +23,16 @@ for line in f:
         # by column
         for x in range(1, 37, 4):
             if (line[x] != ' '):
-                stackHolder[index].append(line[x])
-                index += 1
+                stackHolder[index].append(str(line[x]))
+            index += 1
         stopper += 1
-    # working with move to from instructions
+    # working with move-to-from instructions
     elif stopper == 10:
         elements = getElements(line)
-        stackMover(elements[0], elements[1], elements[2])
+        # print(elements)
+        stackMover(elements[0] - 1, elements[1] - 1, elements[2] - 1)
     # so we can access the txt file after 2 lines
     else:
         stopper += 1
 
+# print(stackHolder)
