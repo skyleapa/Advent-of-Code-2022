fca4e36e760031f23f6817c3eb6cd6e76f8dc2ed
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2022-12-06 03:36:47 -0800

    part 2 solution cleaned up

--- day-5-supply-stacks/supply-stacks-2.py
+++ day-5-supply-stacks/supply-stacks-2.py
@@ -12,20 +12,11 @@ def getElements(line):
 
 # modified from part 1 for part 2
 def stackMover(amount, startIndex, endIndex):
-    # print(stackHolder[startIndex][2])
-    # print(amount)
-    # stackHolder[endIndex].insert(0, stackHolder[startIndex][1])
-
     itVar = amount
     while itVar >= 0:
         stackHolder[endIndex].insert(0, stackHolder[startIndex][itVar])
         stackHolder[startIndex].pop(itVar)
         itVar -= 1
-
-    # for x in range(0, amount):
-    #     print(x)
-    #     stackHolder[endIndex].insert(0, stackHolder[startIndex][0])
-    #     stackHolder[startIndex].pop(0)
 
 stopper = 0
 for line in f:
@@ -41,15 +32,11 @@ for line in f:
     # working with move-to-from instructions
     elif stopper == 10:
         elements = getElements(line)
-        print(elements)
-        print(stackHolder)
         stackMover(elements[0] - 1, elements[1] - 1, elements[2] - 1)
     # so we can access the txt file after 2 lines
     else:
         stopper += 1
 
-# print(stackHolder)
-
-# final solution
+# print final solution
 for x in range(0, 9):
     print(stackHolder[x][0])
