3629a2b607dfd0179db3e75a268127a11099a9ee
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2022-12-07 21:32:40 -0800

    starting day 7

--- day-7-no-space-left-on-device/no-space-left-on-device-1.py
+++ day-7-no-space-left-on-device/no-space-left-on-device-1.py
@@ -4,3 +4,10 @@ f = open("input_darryl.txt", "r")
 # cases: $ cd (cd .. and cd gmz), dir, $ ls, # -> code
 
 # create the tree using 2d array 
+# set up the commands
+# build tree directory
+
+directTree = []
+
+for line in f:
+    
